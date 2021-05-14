6073dcb63a4fa63710601aa8590df7c68e43d61b
Tim Schumacher <timschumi@gmx.de>
2021-05-14 11:23:20 +0200

    Only add bashrc_ext if not already included

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 BASEDIR="$(dirname "$0")"
 
-echo ". \"${BASEDIR}/bashrc_ext\"" >> ~/.bashrc
+if ! grep -q "bashrc_ext" ~/.bashrc; then
+    echo ". \"${BASEDIR}/bashrc_ext\"" >> ~/.bashrc
+fi
 ln -s "${BASEDIR}/vimrc" ~/.vimrc
 ln -s "${BASEDIR}/tmux.conf" ~/.tmux.conf
