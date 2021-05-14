2ef3b444d87ffe1f58ad36dcd3b4feffa431b1f7
Tim Schumacher <timschumi@gmx.de>
2021-05-14 11:23:58 +0200

    Force-create symlinks

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ BASEDIR="$(dirname "$0")"
 if ! grep -q "bashrc_ext" ~/.bashrc; then
     echo ". \"${BASEDIR}/bashrc_ext\"" >> ~/.bashrc
 fi
-ln -s "${BASEDIR}/vimrc" ~/.vimrc
-ln -s "${BASEDIR}/tmux.conf" ~/.tmux.conf
+ln -sf "${BASEDIR}/vimrc" ~/.vimrc
+ln -sf "${BASEDIR}/tmux.conf" ~/.tmux.conf
